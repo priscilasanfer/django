9a0ddb364eca2ba33ad5eb5bc318c1497ca222b3
Priscila Ferreira <priscilasanfer@gmail.com>
2021-12-22 17:42:49 -0300

    Filtro receitas publicadas

--- receitas/models.py
+++ receitas/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from datetime import datetime
 from pessoas.models import Pessoa
 
+
 class Receita(models.Model):
     pessoa = models.ForeignKey(Pessoa, on_delete=models.CASCADE)
     nome_receita = models.CharField(max_length=200)
@@ -11,3 +12,4 @@ class Receita(models.Model):
     rendimento = models.CharField(max_length=100)
     categoria = models.CharField(max_length=100)
     date_receita = models.DateField(default=datetime.now(), blank=True)
+    publicada = models.BooleanField(default=False)
--- receitas/views.py
+++ receitas/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, get_object_or_404, get_list_or_404
 from .models import Receita
 
 def index(request):
-    receitas = Receita.objects.all()
+    receitas = Receita.objects.filter(publicada=True)
 
     dados = {
         'receitas': receitas
